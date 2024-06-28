05f41141899fd96f66115929849f19fb868d283f
JXL Studio <jeremy@jxl.io>
2024-06-28 17:05:37 -0400

    trying again

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,4 +1,3 @@
-import serverless from 'serverless-http';
-import app from './webhook-handler';
+const { handler } = require('./webhook-handler');
 
-export const handler = serverless(app);
+module.exports.handler = handler;
--- netlify/functions/webhook-handler.js
+++ netlify/functions/webhook-handler.js
@@ -1,14 +1,9 @@
-import express from 'express';
-import { json } from 'body-parser';
-import { post } from 'axios';
-
-const app = express();
-app.use(json());
+const axios = require('axios');
 
 const HUBSPOT_API_KEY = Netlify.env.get("HUBSPOT_API_KEY");
 
-app.post('/webhook-handler', async (req, res) => {
-    const contact = req.body;
+const handler = async (event, context) => {
+    const contact = JSON.parse(event.body);
 
     if (contact && contact.properties && contact.objectId) {
         let { firstname, lastname } = contact.properties;
@@ -22,7 +17,7 @@ app.post('/webhook-handler', async (req, res) => {
         }
 
         try {
-            await post(
+            await axios.post(
                 `https://api.hubapi.com/contacts/v1/contact/vid/${contact.objectId}/profile`,
                 {
                     properties: [
@@ -38,14 +33,23 @@ app.post('/webhook-handler', async (req, res) => {
                 }
             );
 
-            res.status(200).json({ firstname, lastname });
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ firstname, lastname })
+            };
         } catch (error) {
             console.error('Error updating contact in HubSpot:', error);
-            res.status(500).send('Failed to update contact');
+            return {
+                statusCode: 500,
+                body: 'Failed to update contact'
+            };
         }
     } else {
-        res.status(400).send('Invalid contact data');
+        return {
+            statusCode: 400,
+            body: 'Invalid contact data'
+        };
     }
-});
+};
 
-export default app;
+module.exports = { handler };
